44587d213cfd51638567fb8d7a3a728d6e1f8a30
crondea <36548937+crondea@users.noreply.github.com>
2018-02-18 15:25:45 -0500

    Add files via upload (#1)

---
+++ senior-design-application/Classes/Background.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InTheHand;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Sockets;
+using System.IO;
+using System.Windows.Forms;
+
+namespace senior_design_application.Classes
+{
+    public class Background
+    {
+
+        //public Background()
+        //{
+        //    const int MAX_MESSAGE_SIZE = 128;
+        //    const int MAX_ATTEMPTS = 3;
+        //}
+
+        public static bool connectAsClient()
+        {
+            BluetoothClient client = new BluetoothClient();
+            MessageBox.Show("Attempting to connect as client.");
+
+
+            return false;
+        }
+
+    }
+
+}
---
+++ senior-design-application/Form1.cs
@@ -0,0 +1,74 @@
+﻿using senior_design_application.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace senior_design_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The purpose of this function is to initialize the system connection to the bluetooth module.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonStartup_Click(object sender, EventArgs e)
+        {
+            if (Background.connectAsClient() == false)
+            {
+                // Connection failed
+            }
+        }
+
+        private void buttonShutdown_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonKickstand_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpdateTable_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBackward_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRightTurn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLeftTurn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
